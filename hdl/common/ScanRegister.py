040bf65b7f952eb437ff39ae4a32336933f587ef
bradfordvt <bradvt59@gmail.com>
2019-10-11 13:58:58 -0400

    Corrected logic errors and fixed support for single bit register that were found during development of cores logic.

--- hdl/common/ScanRegister.py
+++ hdl/common/ScanRegister.py
@@ -30,36 +30,49 @@ def ScanRegister(path, name, si, ce, se, ue, sel, reset, clock, so, di, do, widt
     :param monitor: False=Do not turn on the signal monitors, True=Turn on the signal monitors
     """
     isr = Signal(intbv(0)[width:])
+
+    @always_comb
+    def output():
+        if width == 1:
+            so.next = isr
+        else:
+            so.next = isr[0]
+
     @always(clock.posedge)
     def capture_ff():
         if sel == bool(1) and ce == bool(1):
-            for i in range(width):
-                isr.next[i] = di[i]
+            if width == 1:
+                isr.next[0] = di
+            else:
+                for i in range(width):
+                    isr.next[i] = di[i]
         elif sel == bool(1) and se == bool(1):
-            for i in range(width):
-                if i == 0:
-                    isr.next[i] = si
-                    if width == 1:
-                        so.next = isr[i]
-                elif i == width - 1:
-                    so.next = isr[i]
-                    isr.next[i] = isr[i - 1]
-                else:
-                    isr.next[i] = isr[i - 1]
+            if width == 1:
+                isr.next[0] = si
+            else:
+                # isr.next = concat(si, isr[width:1])
+                i = 1
+                while i < width:
+                    isr.next[i - 1] = isr[i]
+                    i = i + 1
+                isr.next[width - 1] = si
         # else:
-        #     so.next = so
+            # so.next = so
 
-    @always(clock.posedge)
+    @always(clock.negedge)
     def update_ff():
         if reset == bool(0):
             for i in range(width):
                 do.next[i] = bool(0)
         elif sel == bool(1) and ue == bool(1):
-            for i in range(width):
-                do.next[i] = isr[i]
+            if width == 1:
+                do.next[0] = isr[0]
+            else:
+                for i in range(width):
+                    do.next[i] = isr[i]
 
     if not monitor:
-        return capture_ff, update_ff
+        return capture_ff, update_ff, output
     else:
         @instance
         def monitor_si():
@@ -153,15 +166,9 @@ def ScanRegister_tb(monitor=False):
     width = 9
     si = Signal(bool(0))
     so = Signal(bool(0))
-    # di = [Signal(bool(0)) for _ in range(width)]
-    # di[width - 1] = Signal(bool(1))
-    # di[width - 5] = Signal(bool(1))
-    # do = [Signal(bool(0)) for _ in range(width)]
     di = Signal(intbv('010100000')[width:])
     do = Signal(intbv(0)[width:])
-    si_data = [Signal(bool(0)) for _ in range(width)]
-    si_data[width - 5] = Signal(bool(1))
-    si_data[width - 7] = Signal(bool(1))
+    si_data = Signal(intbv('000000101')[width:])
     so_data = [Signal(bool(0)) for _ in range(width)]
     sel = Signal(bool(1))
     ce = Signal(bool(0))
@@ -177,8 +184,8 @@ def ScanRegister_tb(monitor=False):
     def clkgen():
         while True:
             clock.next = not clock
-            yield delay(period//2)
-            
+            yield delay(period // 2)
+
     @instance  # reset signal
     def reset_signal():
         reset.next = 0
@@ -224,25 +231,172 @@ def ScanRegister_tb(monitor=False):
         se.next = H
         yield clock.negedge
         for i in range(width):
-            si.next = si_data[width - 1 - i]
+            si.next = si_data[i]
             yield clock.posedge
             yield clock.negedge
-            so_data[width - 1 - i].next = so
+            so_data[i].next = so
         # Write Update value
         se.next = L
         ue.next = H
         yield clock.negedge
         yield clock.posedge
-        for j in range(width):
-            if j == 3 or j == 1:
-                assert(so_data[width - 1 - j] == bool(1))
+        j = width - 1
+        while j > -1:
+            # print("so_data[", j, "] = ", so_data[j])
+            if j == 5 or j == 7:
+                assert (so_data[j] == bool(1))
             else:
-                assert(so_data[width - 1 - j] == bool(0))
+                assert (so_data[j] == bool(0))
+            j = j - 1
         for j in range(width):
-            if j == 4 or j == 6:
-                assert(do[width - 1 - j] == bool(1))
+            # print("do[", j, "] = ", do[j])
+            if j == 0 or j == 2:
+                assert (do[j] == bool(1))
             else:
-                assert(do[width - 1 - j] == bool(0))
+                assert (do[j] == bool(0))
+        assert (do == intbv('000000101'))
+
+        raise StopSimulation()
+
+    return sreg_inst, clkgen, stimulus, reset_signal, print_data
+
+
+@block
+def ScanRegister1_tb(monitor=False):
+    """
+    Test bench interface for a quick test of the operation of the design
+    :param monitor: False=Do not turn on the signal monitors, True=Turn on the signal monitors
+    :return: A list of generators for this logic
+    """
+    width = 1
+    si = Signal(bool(0))
+    so = Signal(bool(0))
+    di = Signal(intbv(0)[width:])
+    do = Signal(intbv(0)[width:])
+    si_data = Signal(intbv('0')[width:])
+    so_data = [Signal(bool(0)) for _ in range(width)]
+    sel = Signal(bool(1))
+    ce = Signal(bool(0))
+    se = Signal(bool(0))
+    ue = Signal(bool(0))
+    reset = Signal(bool(0))
+    clock = Signal(bool(0))
+    t = 0
+    sreg_inst = ScanRegister('TOP', 'ScanRegister1', si, ce, se, ue, sel, reset, clock, so, di, do, width=width,
+                             monitor=monitor)
+
+    @instance
+    def clkgen():
+        while True:
+            clock.next = not clock
+            yield delay(period // 2)
+
+    @instance  # reset signal
+    def reset_signal():
+        reset.next = 0
+        yield delay(period)
+        reset.next = 1
+
+    # print simulation data to file
+    file_data = open("ScanRegister1.csv", 'w')  # file for saving data
+    # print header to file
+    print("{0},{1},{2},{3},{4},{5},{6},{7}".format("si", "ce", "se", "ue", "sel", "so", "di", "do"),
+          file=file_data)
+
+    # print data on each clock
+    @always(clock.posedge)
+    def print_data():
+        """
+        """
+        # print in file
+        # print.format is not supported in MyHDL 1.0
+        print(si, ",", ce, ",", se, ",", ue, ",", sel, ",", so, ",", di, ",", do, file=file_data)
+
+    @instance
+    def stimulus():
+        """
+        Not true IJTAG protocol, but used to exercise the state machine with the fewest cycles
+        :return:
+        """
+        H = bool(1)
+        L = bool(0)
+        # # Reset the instrument
+        # reset.next = bool(0)
+        # yield delay(period)
+        # reset.next = bool(1)
+        yield delay(period)
+        # Start the Capture transition operation
+        yield clock.posedge
+        # Write Capture value
+        ce.next = H
+        yield clock.negedge
+        yield clock.posedge
+        # Write Shift value
+        ce.next = L
+        se.next = H
+        yield clock.negedge
+        si.next = L
+        yield clock.posedge
+        yield clock.negedge
+        so_data[0].next = so
+        # Write Update value
+        se.next = L
+        ue.next = H
+        yield clock.negedge
+        yield clock.posedge
+        assert (so_data[0] == L)
+        assert (do[0] == L)
+        # Write Capture value
+        ue.next = L
+        ce.next = H
+        yield clock.negedge
+        yield clock.posedge
+        # Write Shift value
+        ce.next = L
+        se.next = H
+        yield clock.negedge
+        si.next = H
+        yield clock.posedge
+        yield clock.negedge
+        so_data[0].next = so
+        # Write Update value
+        se.next = L
+        ue.next = H
+        yield clock.negedge
+        yield clock.posedge
+        assert (so_data[0] == L)
+        assert (do[0] == H)
+        # Write Capture value
+        ue.next = L
+        ce.next = H
+        yield clock.negedge
+        yield clock.posedge
+        # Write Shift value
+        ce.next = L
+        se.next = H
+        yield clock.negedge
+        si.next = H
+        yield clock.posedge
+        yield clock.negedge
+        so_data[0].next = so
+        # Write Shift value
+        ce.next = L
+        se.next = H
+        yield clock.negedge
+        si.next = L
+        yield clock.posedge
+        yield clock.negedge
+        so_data[0].next = so
+        # Write Update value
+        se.next = L
+        ue.next = H
+        yield clock.negedge
+        yield clock.posedge
+        ue.next = L
+        assert (so_data[0] == H)
+        assert (do[0] == L)
+        yield clock.negedge
+        yield clock.posedge
 
         raise StopSimulation()
 
@@ -287,6 +441,9 @@ def main():
     tb = ScanRegister_tb(monitor=True)
     tb.config_sim(trace=True)
     tb.run_sim()
+    tb0 = ScanRegister1_tb(monitor=True)
+    tb0.config_sim(trace=True)
+    tb0.run_sim()
     convert()
 
 
