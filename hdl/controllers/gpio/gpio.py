da26fc19b9099693a83d93ce0e848c568e3b28b1
bradfordvt <50559113+bradfordvt@users.noreply.github.com>
2019-12-11 11:34:21 -0500

    Initial release.

---
+++ hdl/controllers/gpio/gpio.py
@@ -0,0 +1,11 @@
+"""
+Copyright (c) 2019 Bradford G. Van Treuren
+See the licence file in the top directory
+
+GPIO 8-bit register
+"""
+from myhdl import *
+from hdl.hosts.gpiohost.gpio_master import gpio_master
+
+
+def gpio(clk, rst, gpio_i, gpio_o, gpio_e, )
