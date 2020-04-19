df14ead4174573b4eed6816edfecc919f519f802
bradfordvt <50559113+bradfordvt@users.noreply.github.com>
2020-04-19 15:07:14 -0400

    Added support for DisplayPanel application.

--- hdl/instruments/led/led.py
+++ hdl/instruments/led/led.py
@@ -12,6 +12,73 @@ import time
 import tkinter as tk
 from PIL import ImageTk, Image
 from myhdl import *
+from socket import *
+
+HEADER_LENGTH = 10
+
+
+class LEDDisplay:
+    display_instance = None
+    client = socket(AF_INET, SOCK_STREAM)
+    ip = 'localhost'
+    server = (ip, 285)
+    try:
+        client.connect(server)
+        print("Connected to server!")
+    except:
+        client = None
+
+    @staticmethod
+    def display_factory():
+        if LEDDisplay.display_instance is None:
+            LEDDisplay.display_instance = LEDDisplay()
+        return LEDDisplay.display_instance
+
+    def __init__(self):
+        pass
+
+    def send_message(self, message):
+        print("Sending message: ", message)
+        message = message.encode('utf-8')
+        message_header = f"{len(message):<{HEADER_LENGTH}}".encode('utf-8')
+        self.client.send(message_header + message)
+
+    def create_led(self, name):
+        try:
+            if self.client is not None:
+                text = "NEW_LED " + name
+                print(text)
+                self.send_message(text)
+        except:
+            pass
+
+    def led_on(self, name):
+        try:
+            if self.client is not None:
+                text = "LED_ON " + name
+                print(text)
+                self.send_message(text)
+        except:
+            pass
+
+    def led_off(self, name):
+        try:
+            if self.client is not None:
+                text = "LED_OFF " + name
+                print(text)
+                self.send_message(text)
+        except:
+            pass
+
+    def quit(self):
+        try:
+            if self.client is not None:
+                text = "QUIT"
+                print(text)
+                self.send_message(text)
+                self.client.close()
+        except:
+            pass
 
 
 class LEDThread (threading.Thread):
@@ -21,18 +88,10 @@ class LEDThread (threading.Thread):
         self.qlock = qlock
         self.name = name
         self.q = q
-        self.window = None
-        ipath = os.path.dirname(__file__)
-        self.path_off = os.path.join(ipath, "off_led.png")
-        self.path_on = os.path.join(ipath, "on_led.png")
-        # Creates a Tkinter-compatible photo image, which can be used everywhere Tkinter expects an image object.
-        self.img_off = None
-        self.img_on = None
-        self.panel = None
+        self.display = LEDDisplay.display_factory()
+        self.display.create_led(name)
         self.thread1 = None
-        self.thread2 = None
-        self.running = None
-        self.label = None
+        self.running = False
 
     def run(self):
         print("Starting " + self.name)
@@ -44,17 +103,8 @@ class LEDThread (threading.Thread):
         self.running = True
         self.thread1 = threading.Thread(target=self.control_led)
         self.thread1.start()
-        self.thread2 = threading.Thread(target=self.display_window)
-        self.thread2.start()
-        # Start the GUI
-        # self.window.mainloop()
-        print("After thread2.start")
-        # self.thread1.stop()
-        # self.stop()
         self.thread1.join()
         print("After join")
-        self.thread2.join()
-        print("After thread2.join")
 
     def stop(self):
         self.running = False
@@ -69,39 +119,17 @@ class LEDThread (threading.Thread):
                     self.qlock.release()
                     time.sleep(1)
                     if msg == "ON":
-                        self.panel.configure(image=self.img_on)
-                        self.panel.image = self.img_on
+                        self.display.led_on(self.name)
                     elif msg == "OFF":
-                        self.panel.configure(image=self.img_off)
-                        self.panel.image = self.img_off
+                        self.display.led_off(self.name)
                     else:
                         print("Stop message received")
                         self.running = False
                 except queue.Empty:
                     pass
-        self.window.quit()
+        self.display.quit()
         print("After window.quit")
         print("Exiting control_led")
-
-    def display_window(self):
-        # self.window = tk.Tk()
-        self.window = tk.Toplevel()
-        self.window.title(self.name)
-        # self.window.geometry("300x300")
-        self.window.geometry("150x150")
-        self.window.configure(background='grey')
-        self.img_off = ImageTk.PhotoImage(Image.open(self.path_off))
-        self.img_on = ImageTk.PhotoImage(Image.open(self.path_on))
-        self.label = tk.Label(self.window, text=self.name)
-        self.label.pack(side="top", fill="both", expand="yes")
-        # The Label widget is a standard Tkinter widget used to display a text or image on the screen.
-        self.panel = tk.Label(self.window, image=self.img_off)
-        # The Pack geometry manager packs widgets in rows or columns.
-        self.panel.pack(side="bottom", fill="both", expand="yes")
-        # Start the GUI
-        print("Entering display_window")
-        self.window.mainloop()
-        print("Exiting display_window")
 
 
 class LED:
