6ebd8a402d8b3028de899af07459a736eb8cee90
bradfordvt <50559113+bradfordvt@users.noreply.github.com>
2020-05-25 21:20:14 -0400

    Initial release.

---
+++ hdl/boards/jtagtest/jtag2test.py
@@ -0,0 +1,112 @@
+"""
+Copyright (c) 2020 Bradford G. Van Treuren
+See the licence file in the top directory
+"""
+
+from myhdl import *
+import threading
+
+from hdl.experimental.pseudo_led import pseudo_led
+from hdl.boards.common.AbstractBoard import AbstractBoard
+
+
+class JTAG2Test(AbstractBoard):
+    def __init__(self):
+        super().__init__()
+        # # SYSCON Signals
+        # self.clk_o = None
+        # self.rst_o = None
+        # # GPIO Signals
+        # self.i_gpio = None
+        # self.o_gpio = None
+        # # JTAG Signals
+        # self.tck = None
+        # self.tms = None
+        # self.trst = None
+        # self.tdi = None
+        # self.tdo = None
+        # # I2C Signals
+        # self.sck_o = None
+        # self.sck_i = None
+        # self.sck_e = None
+        # self.sda_o = None
+        # self.sda_i = None
+        # self.sda_e = None
+        # # SPI Signals
+        # self.sclk = None
+        # self.mosi = None
+        # self.miso = None
+        # self.ss = None
+        # # TPSP Signals
+        # self.tp_sck = None
+        # self.tp_i = None
+        # self.tp_o = None
+        # self.tp_e = None
+
+        self.clk = Signal(bool(0))
+        self.state0 = Signal(bool(0))
+        self.state1 = Signal(bool(0))
+        self.state2 = Signal(bool(0))
+        self.state3 = Signal(bool(0))
+        self.state4 = Signal(bool(0))
+
+        self.led0 = None
+        self.led1 = None
+        self.led2 = None
+        self.led3 = None
+        self.led4 = None
+
+    def configure_syscon(self, clk_o, rst_o):
+        self.clk_o = clk_o
+        self.rst_o = rst_o
+
+    # def configure_gpio(self, i_gpio, o_gpio):
+    #     self.i_gpio = i_gpio
+    #     self.o_gpio = o_gpio
+    #
+    # def configure_jtag(self, tdi, tck, tms, trst, tdo):
+    #     self.tdi = tdi
+    #     self.tck = tck
+    #     self.tms = tms
+    #     self.trst = trst
+    #     self.tdo = tdo
+    #
+    # def configure_i2c(self, sck_o, sck_i, sck_e, sda_o, sda_i, sda_e):
+    #     self.sck_o = sck_o
+    #     self.sck_i = sck_i
+    #     self.sck_e = sck_e
+    #     self.sda_o = sda_o
+    #     self.sda_i = sda_i
+    #     self.sda_e = sda_e
+    #
+    # def configure_spi(self, sclk, mosi, miso, ss):
+    #     self.sclk = sclk
+    #     self.mosi = mosi
+    #     self.miso = miso
+    #     self.ss = ss
+
+    @block
+    def rtl(self):
+
+        self.led0 = pseudo_led(self.clk, self.state0, color="WHITE")
+        self.led1 = pseudo_led(self.clk, self.state1, color="RED")
+        self.led2 = pseudo_led(self.clk, self.state2, color="GREEN")
+        self.led3 = pseudo_led(self.clk, self.state3, color="YELLOW")
+        self.led4 = pseudo_led(self.clk, self.state4, color="BLUE")
+
+        # build up the netlist for the board here
+        @always_comb
+        def netlist():
+            # Wire the LEDs to the GPIO Signals
+            self.state0.next = self.o_gpio[0]
+            self.state1.next = self.o_gpio[1]
+            self.state2.next = self.o_gpio[2]
+            self.state3.next = self.o_gpio[3]
+            self.state4.next = self.o_gpio[4]
+            self.i_gpio.next = self.o_gpio
+            # Wire the LED controller clock to the WB clock
+            self.clk.next = self.clk_o
+            # JTAG loopback
+            self.tdo.next = self.tdi
+
+        return self.led0, self.led1, self.led2, self.led3, self.led4, netlist
---
+++ hdl/boards/jtagtest/jtag2test_bench.py
@@ -0,0 +1,312 @@
+"""
+Copyright (c) 2019 Bradford G. Van Treuren
+See the licence file in the top directory
+"""
+
+import binascii
+from time import sleep
+from hdl.boards.jtagtest.jtag2test import JTAG2Test
+from hdl.ate.ate import ATE
+from hdl.hosts.jtaghost.tapsim import *
+from hdl.boards.common.BoardGPIOInterface import BoardGPIOInterface
+from hdl.boards.common.BoardJTAGInterface import BoardJTAGInterface
+
+
+def write_vector(ate_inst, addr, data):
+    """
+
+    :param ate_inst:
+    :param addr:
+    :param data:
+    :return:
+    """
+    assert(addr < 0x3000)
+    wb_addr = 0x00003000 + addr
+    # print("Writing ", hex(data & 0xFF), " to address ", hex(wb_addr))
+    ret = ate_inst.write(wb_addr, data & 0xFF)
+    if ret == False:
+        print("Write Error: ", ate_inst.get_error())
+    assert(ret == True)
+
+
+def read_vector(ate_inst, addr):
+    """
+
+    :param ate_inst:
+    :param addr:
+    :return:
+    """
+    assert(addr < 0x3000)
+    wb_addr = 0x00003000 + addr
+    ret = ate_inst.read(wb_addr)
+    if ret == False:
+        print("Read Error: ", ate_inst.get_error())
+    assert(ret == True)
+    value = ate_inst.get_value() & 0xFF
+    # print("Read ", hex(value), " from address ", hex(wb_addr))
+    return value
+
+
+def set_chain_length(ate_inst, count):
+    """
+
+    :param ate_inst:
+    :param count:
+    :return:
+    """
+    wb_addr = 0x00003000 + 0x402
+    assert(ate_inst.write(wb_addr, count & 0xFFFF))
+
+
+def set_scan_state(ate_inst, state):
+    """
+
+    :param ate_inst:
+    :param state:
+    :return:
+    """
+    wb_addr = 0x00003000 + 0x400
+    assert (ate_inst.write(wb_addr, state & 0xF))
+
+
+def set_end_state(ate_inst, state):
+    """
+
+    :param ate_inst:
+    :param state:
+    :return:
+    """
+    wb_addr = 0x00003000 + 0x401
+    assert (ate_inst.write(wb_addr, state & 0xF))
+
+
+def set_control_register(ate_inst, value):
+    """
+
+    :param ate_inst:
+    :param value:
+    :return:
+    """
+    wb_addr = 0x00003000 + 0x403
+    assert (ate_inst.write(wb_addr, value & 0x1))
+
+
+def get_status_register(ate_inst):
+    """
+
+    :param ate_inst:
+    :return:
+    """
+    wb_addr = 0x00003000 + 0x404
+    assert (ate_inst.read(wb_addr))
+    return ate_inst.get_value() & 0x1
+
+
+def set_command(ate_inst, state):
+    """
+
+    :param ate_inst:
+    :param state:
+    :return:
+    """
+    wb_addr = 0x00003000 + 0x405
+    assert (ate_inst.write(wb_addr, state & 0xF))
+
+
+def scan_vector(ate_inst, tdi_vector, count, start, end):
+    """
+
+    :param ate_inst:
+    :param tdi_vector:
+    :param count:
+    :param start:
+    :param end:
+    :return:
+    """
+    # Fill the TAPSim data buffer memory with tdi data
+    data_width = 8
+    addr_width = 10
+    num_full_words = int(count // data_width)
+    tdo_vector = bytearray((count + data_width - 1) // data_width)
+    remainder = count % data_width
+    addr = 0
+    for i in range(num_full_words):
+        data = tdi_vector[i]
+        write_vector(ate_inst, addr, data)
+        addr = addr + 1
+    # Now write out the remaining bits that may be a partial word in size, but a full word needs to be written
+    if remainder > 0:
+        data = tdi_vector[num_full_words]
+        write_vector(ate_inst, addr, data)
+    # Now start the scan operation
+    set_chain_length(ate_inst, count)
+    set_scan_state(ate_inst, start)
+    set_end_state(ate_inst, end)
+    set_command(ate_inst, SCAN)
+    set_control_register(ate_inst, 0x1)  # Start the scan
+    status = get_status_register(ate_inst)
+    while status != 0:
+        status = get_status_register(ate_inst)
+    set_control_register(ate_inst, 0x0)  # Stop the scan/Reset for next scan cycle trigger
+    # Scan completed, now fetch the captured data
+    addr = 0
+    for i in range(num_full_words):
+        data = read_vector(ate_inst, addr)
+        tdo_vector[i] = int(data)
+        addr = addr + 1
+    # Now read out the remaining bits that may be a partial word in size, but a full word needs to be read
+    if remainder > 0:
+        data = read_vector(ate_inst, addr)
+        # print(">>tdo_vector = ", tdo_vector)
+        # print(">>num_full_words = ", num_full_words)
+        # print(">>data = ", data)
+        tdo_vector[num_full_words] = int(data)
+    return tdo_vector
+
+
+def ba_scan_ir(ate_inst, tdi_vector, count):
+    """
+    Scan the vector to the TAP with the IR data and capture the response in tdo_vector
+    :param ate_inst:
+    :param tdi_vector: Data to be shifted out as bytearray
+    :param count: number of bits to shift
+    :param tdo_vector: Data to be captured as bytearray
+    :return:
+    """
+    start = SI_SHIFT_IR
+    end = SI_RUN_TEST_IDLE
+    return scan_vector(ate_inst, tdi_vector, count, start, end)
+
+
+def ba_scan_dr(ate_inst, tdi_vector, count):
+    """
+    Scan the vector to the TAP with the DR data and capture the response in tdo_vector
+    :param ate_inst:
+    :param tdi_vector: Data to be shifted out as bytearray
+    :param count: number of bits to shift
+    :return:
+    """
+    start = SI_SHIFT_DR
+    end = SI_RUN_TEST_IDLE
+    return scan_vector(ate_inst, tdi_vector, count, start, end)
+
+
+def scan_ir(ate_inst, count, tdi_string):
+    """
+
+    :param ate_inst:
+    :param count:
+    :param tdi_string:
+    :return: tdo_string
+    """
+    if len(tdi_string) % 2:
+        tdi_string = '0' + tdi_string
+    # print("tdi_string = ", tdi_string)
+    tdi_vector = bytearray.fromhex(tdi_string)
+    # print("tdi_vector = ", tdi_vector)
+    if len(tdi_vector) > 1:
+        tdi_vector.reverse()
+        # print("tdi_vector = ", tdi_vector)
+    tdo_vector = ba_scan_ir(ate_inst, tdi_vector, count)
+    # print("tdo_vector = ", tdo_vector)
+    if len(tdo_vector) > 1:
+        tdo_vector.reverse()
+    tdo_string = tdo_vector.hex().upper()
+    # print("tdo_string = ", tdo_string)
+    if len(tdo_string) * 4 > count:
+        tdo_string = tdo_string[1:]
+        # print("tdo_string = ", tdo_string)
+    return tdo_string
+
+
+def scan_dr(ate_inst, count, tdi_string):
+    """
+
+    :param ate_inst:
+    :param count:
+    :param tdi_string:
+    :return: tdo_string
+    """
+    if len(tdi_string) % 2:
+        tdi_string = '0' + tdi_string
+    # print("tdi_string = ", tdi_string)
+    tdi_vector = bytearray.fromhex(tdi_string)
+    # print("tdi_vector = ", tdi_vector)
+    if len(tdi_vector) > 1:
+        tdi_vector.reverse()
+        # print("tdi_vector = ", tdi_vector)
+    tdo_vector = ba_scan_dr(ate_inst, tdi_vector, count)
+    # print("tdo_vector = ", tdo_vector)
+    if len(tdo_vector) > 1:
+        tdo_vector.reverse()
+    tdo_string = tdo_vector.hex().upper()
+    # print("tdo_string = ", tdo_string)
+    if len(tdo_string) * 4 > count:
+        tdo_string = tdo_string[1:]
+        # print("tdo_string = ", tdo_string)
+    return tdo_string
+
+
+def jtag2test_bench():
+    gpio_if = BoardGPIOInterface()
+    jtag_if = BoardJTAGInterface()
+    board_inst = JTAG2Test()
+    board_inst.configure_jtag(jtag_if)
+    board_inst.configure_gpio(gpio_if)
+    ate_inst = ATE(board_inst)
+    ate_inst.configure_jtag2(jtag_if)
+    ate_inst.configure_gpio(gpio_if)
+    ate_inst.start_simulation()
+    sleep(1)
+
+    # GPIO Test
+    assert(ate_inst.write(0x00001800, 0x00000000))
+    assert(ate_inst.read(0x00001800))
+    assert(ate_inst.get_value() == 0x00000000)
+    assert(ate_inst.write(0x00001800, 0x00000015))
+    assert(ate_inst.read(0x00001800))
+    assert(ate_inst.get_value() == 0x00150015)
+    assert(ate_inst.write(0x00001800, 0x0000000A))
+    assert(ate_inst.read(0x00001800))
+    assert(ate_inst.get_value() == 0x000A000A)
+    assert(ate_inst.write(0x00001800, 0x00000000))
+    assert(ate_inst.read(0x00001800))
+    assert(ate_inst.get_value() == 0x00000000)
+
+    # JTAG Test
+    assert(ate_inst.write(0x00001800, 0x00000001))  # Turn on WHITE LED to indicate scan start
+    tdo = scan_ir(ate_inst, 8, '55')
+    # print("tdo = ", tdo)
+    assert(tdo == '55')
+    assert(ate_inst.write(0x00001800, 0x00000002))  # Turn on RED LED to indicate scan start
+    tdo = scan_ir(ate_inst, 12, '0A55')
+    assert(tdo == 'A55')
+    assert(ate_inst.write(0x00001800, 0x00000004))  # Turn on GREEN LED to indicate scan start
+    tdo = scan_ir(ate_inst, 12, '5AA')
+    assert(tdo == '5AA')
+    assert(ate_inst.write(0x00001800, 0x00000008))  # Turn on YELLOW LED to indicate scan start
+    tdo = scan_dr(ate_inst, 8, '55')
+    assert(tdo == '55')
+    assert(ate_inst.write(0x00001800, 0x00000010))  # Turn on BLUE LED to indicate scan start
+    tdo = scan_dr(ate_inst, 12, 'AAA')
+    assert(tdo == 'AAA')
+    assert(ate_inst.write(0x00001800, 0x00000011))  # Turn on BLUE & WHITE LEDs to indicate scan start
+    tdo = scan_dr(ate_inst, 12, 'A55')
+    assert(tdo == 'A55')
+    assert(ate_inst.write(0x00001800, 0x00000012))  # Turn on BLUE & RED LEDs to indicate scan start
+    tdo = scan_dr(ate_inst, 12, '5AA')
+    assert(tdo == '5AA')
+    assert(ate_inst.write(0x00001800, 0x00000014))  # Turn on BLUE & GREEN LEDs to indicate scan start
+    tdo = scan_dr(ate_inst, 16 * 4, '0123456789ABCDEF')
+    assert(tdo == '0123456789ABCDEF')
+
+    # End the simulation
+    assert(ate_inst.terminate())
+
+
+def main():
+    jtag2test_bench()
+
+
+if __name__ == '__main__':
+    main()
